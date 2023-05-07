32e4ed5aa8296064c119bed2ab767ef99c634118
fernando_emercini@outlook.com <fernando_emercini@outlook.com>
2023-05-06 22:24:20 -0300

    Primeiros Scripts

---
+++ src/test/java/steps/DescontosStep.java
@@ -0,0 +1,27 @@
+package steps;
+
+import cucumber.api.java.es.Dado;
+import cucumber.api.java.it.Quando;
+import cucumber.api.java.pt.Então;
+
+public class DescontosStep {
+    @Dado("^que estou no site da qazando$")
+    public void que_estou_no_site_da_qazando(){
+        
+    }
+
+    @Quando("^eu preencho meu e-mail$")
+    public void eu_preencho_meu_e_mail(){
+
+    }
+
+    @Quando("^clico em ganhar cupom$")
+    public void clico_em_ganhar_cupom(){
+
+    }
+
+    @Então("^eu vejo o código de desconto$")
+    public void eu_vejo_o_código_de_desconto(){
+
+    }
+}
